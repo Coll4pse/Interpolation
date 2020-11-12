b712260a767cf6dd18f31f2be73e93adcc93983a
Stepanov Mikhail <mihanoid10@yandex.ru>
2020-11-12 22:27:49 +0500

    Implement Lagrange Method

--- LagrangeMethod.cs
+++ LagrangeMethod.cs
@@ -6,7 +6,27 @@ namespace Interpolation
     {
         public static Polynomial Evaluate(double[] x, double[] y)
         {
-            throw new NotImplementedException();
+            var polynomial = new Polynomial();
+            for (int i = 0; i < x.Length; i++)
+            {
+                polynomial += BasicPolynomial(x, i) * y[i];
+            }
+            return polynomial;
+        }
+
+        private static Polynomial BasicPolynomial(double[] array, int i)
+        {
+            Polynomial basicPolynomial = new Polynomial(1);
+
+            for (int j = 0; j < array.Length; j++)
+            {
+                if (j != i)
+                {
+                    basicPolynomial *= new Polynomial(-array[j], 1) / (array[i] - array[j]);
+                }
+            }
+
+            return basicPolynomial;
         }
     }
 }
